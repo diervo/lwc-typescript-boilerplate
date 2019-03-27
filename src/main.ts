74e95a3ed0484b48b94a3cc822250a14356b72c8
Diego Ferreiro Val <dval@salesforce.com>
2019-03-27 11:09:52 -0400

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import App from "lwc/helloWorld";
+import { createElement } from "lwc";
+
+const app = createElement("lwc-hello-world", { is: App });
+
+function test(dom: Element, str: string) {
+    return dom.getAttribute(str);
+}
+
+// eslint-disable-next-line @lwc/lwc/no-document-query
+const container = document.getElementById("main");
+test(container, "test");
+container.appendChild(app);
